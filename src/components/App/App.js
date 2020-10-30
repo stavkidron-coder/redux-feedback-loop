291fb2bf3b9dc0fc6afbee04d63d0339b3ab0f50
Stav Kidron <kidronstav@gmail.com>
2020-10-30 16:57:36 -0500

    set up routes and create a test reducer that will be changed/deleted

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {HashRouter as Router, Route} from 'react-router-dom';
 
 import Feeling from '../Feeling/Feeling';
 import Understanding from '../Understanding/Understanding';
@@ -10,19 +11,26 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br/>
 
-        <Feeling/>
-        <Understanding/>
-        <Support/>
-        <Comments/>
+          {/* <Feeling/>
+          <Understanding/>
+          <Support/>
+          <Comments/> */}
 
-      </div>
+          <Route exact path="/" component={Feeling}/>
+          <Route exact path="/understanding" component={Understanding}/>
+          <Route exact path="/support" component={Support}/>
+          <Route exact path="/comments" component={Comments}/>
+
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// just a test to get up and running. Replace or delete when setting up for actual project
+const testReducer = (state=0) => {
+    console.log('Hello from testReducer');
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        testReducer
+    })
+)
+
+
+ReactDOM.render(<Provider store={storeInstance}> <App /> </Provider>, document.getElementById('root'));
 registerServiceWorker();
