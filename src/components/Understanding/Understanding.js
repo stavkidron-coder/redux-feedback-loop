fa0fc3cc606226c1fd3342e4143c5fcf7f0efbbf
Stav Kidron <kidronstav@gmail.com>
2020-10-31 14:38:22 -0500

    Understanding page functionality working

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,11 +1,48 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
 class Understanding extends Component {
+
+    state = {
+        understanding: 0
+    }
+
+    handleInput = (event) => {
+        // console.log(this.state.understanding); //console log for test
+        this.setState({
+            understanding: Number(event.target.value)
+        });
+    }
+
+    nextPage = () => {
+        // send answer to reduxState
+        this.props.dispatch({type: 'UNDERSTANDING', payload: this.state.understanding});
+
+        //Navigate to next page
+        this.props.history.push('/support');
+    }
+
     render(){
         return(
-            <p>Understanding Page</p>
+            <div>
+                <h2>How well are you understanding the content?</h2>
+
+                <label htmlFor="understandingInput">Choose a number between 1 and 6: </label>
+                <input
+                    id="understandingInput"
+                    type="number"
+                    onChange={(event) => this.handleInput(event)}
+                    min="1"
+                    max="6">
+                    </input>
+
+                    <button onClick={this.nextPage}>Next</button>
+            </div>
         )
     }
 }
 
-export default Understanding;
+const putReduxStateOnProps = (reduxState) => ({
+    reduxState
+})
+export default connect(putReduxStateOnProps)(Understanding);
